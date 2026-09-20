595b17a689a9fa7e0a22f67db00f8859da1b1105
pemunson90-cmd <p.e.munson90@gmail.com>
2026-09-19 22:55:00 -0400

    M12C: bump application version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.pocketsteward.app"
         minSdk = 30
         targetSdk = 36
-        versionCode = 15
-        versionName = "0.15.0-milestone12a"
+        versionCode = 16
+        versionName = "0.16.0-milestone12c"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
