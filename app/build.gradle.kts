cd2ff0f6b2e033dd60d47595d347c32ea7666dde
pemunson90-cmd <p.e.munson90@gmail.com>
2026-09-21 12:25:19 -0400

    V1: stamp 1.0.0-rc1 update build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.pocketsteward.app"
         minSdk = 30
         targetSdk = 36
-        versionCode = 30
-        versionName = "0.30.0-m13-major"
+        versionCode = 31
+        versionName = "1.0.0-rc1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
