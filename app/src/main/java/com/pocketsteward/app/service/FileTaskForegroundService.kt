8d716a404e695b225ff748912076fccda79a3f99
pemunson90-cmd <p.e.munson90@gmail.com>
2026-09-21 17:52:26 -0400

    V1.1: route file-task notifications to Tasks

--- app/src/main/java/com/pocketsteward/app/service/FileTaskForegroundService.kt
+++ app/src/main/java/com/pocketsteward/app/service/FileTaskForegroundService.kt
@@ -8,6 +8,7 @@ import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.content.pm.ServiceInfo
+import android.net.Uri
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import androidx.core.app.ServiceCompat
@@ -182,6 +183,7 @@ class FileTaskForegroundService : Service() {
             this,
             0,
             Intent(this, MainActivity::class.java).apply {
+                data = Uri.parse("pocketsteward://tasks")
                 flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
             },
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE,
@@ -224,7 +226,9 @@ class FileTaskForegroundService : Service() {
         val openIntent = PendingIntent.getActivity(
             this,
             0,
-            Intent(this, MainActivity::class.java),
+            Intent(this, MainActivity::class.java).apply {
+                data = Uri.parse("pocketsteward://tasks")
+            },
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE,
         )
         manager.notify(
