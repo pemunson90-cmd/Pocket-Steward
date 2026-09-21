bedc9cc1f06b43cdd5a140012fa5737874b22dc2
pemunson90-cmd <p.e.munson90@gmail.com>
2026-09-20 23:46:37 -0400

    M13.3Q2A: bump search review repair build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.pocketsteward.app"
         minSdk = 30
         targetSdk = 36
-        versionCode = 23
-        versionName = "0.21.2-m13-3q1"
+        versionCode = 24
+        versionName = "0.21.3-m13-3q2a"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
