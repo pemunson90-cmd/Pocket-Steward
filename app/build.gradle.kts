927af0f67e7dd485c770c61da8d34d41dd5bd7f5
pemunson90-cmd <p.e.munson90@gmail.com>
2026-09-21 00:15:15 -0400

    Q2: bump integrated quality sprint build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.pocketsteward.app"
         minSdk = 30
         targetSdk = 36
-        versionCode = 24
-        versionName = "0.21.3-m13-3q2a"
+        versionCode = 25
+        versionName = "0.22.0-m13-3q2-quality"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
