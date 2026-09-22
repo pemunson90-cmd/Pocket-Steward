d47c8132437322669677e0b27cfc89d7430a0656
pemunson90-cmd <p.e.munson90@gmail.com>
2026-09-22 13:58:18 -0400

    V1.1: add in-memory Room support for end-to-end acceptance tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -133,6 +133,7 @@ dependencies {
     testImplementation("com.google.truth:truth:1.4.4")
 
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.room:room-testing:2.8.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 }
