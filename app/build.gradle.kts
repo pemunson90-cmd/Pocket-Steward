ffb91c9a9db14b1fce98e52391f37910b6211d92
pemunson90-cmd <p.e.munson90@gmail.com>
2026-09-22 17:12:19 -0400

    Release: stamp Pocket Steward 1.1.0-rc1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.pocketsteward.app"
         minSdk = 30
         targetSdk = 36
-        versionCode = 32
-        versionName = "1.0.0-rc2"
+        versionCode = 33
+        versionName = "1.1.0-rc1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
